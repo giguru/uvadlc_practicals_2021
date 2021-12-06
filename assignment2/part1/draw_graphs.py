f5df902cfa2db9e281edc3c5051ac5832703226c
Giguru Scheuer <giguru@uw-s.nl>
2021-12-06 21:15:20 +0100

    CNN graphs at epoch 250

--- assignment2/part1/draw_graphs.py
+++ assignment2/part1/draw_graphs.py
@@ -2,36 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # results from LISA
-results_resnet18 = {
-    'none': 0.6578,
-    'gaussian_noise-1': 0.6487, 'gaussian_noise-2': 0.6315, 'gaussian_noise-3': 0.5957,
-    'gaussian_noise-4': 0.5329, 'gaussian_noise-5': 0.4019, 'gaussian_blur-1': 0.6565,
-    'gaussian_blur-2': 0.6395, 'gaussian_blur-3': 0.6178, 'gaussian_blur-4': 0.5502,
-    'gaussian_blur-5': 0.4396, 'contrast-1': 0.622, 'contrast-2': 0.5362, 'contrast-3': 0.4428,
-    'contrast-4': 0.3542, 'contrast-5': 0.2819, 'jpeg-1': 0.6537, 'jpeg-2': 0.6507, 'jpeg-3': 0.6508,
-    'jpeg-4': 0.643, 'jpeg-5': 0.6114
-}
-results_resnet34 = {
-    'none': 0.6067,
-    'gaussian_noise-1': 0.6029, 'gaussian_noise-2': 0.5971, 'gaussian_noise-3': 0.5732, 'gaussian_noise-4': 0.534, 'gaussian_noise-5': 0.4273,
-    'gaussian_blur-1': 0.6067, 'gaussian_blur-2': 0.5956, 'gaussian_blur-3': 0.5721, 'gaussian_blur-4': 0.523, 'gaussian_blur-5': 0.4416,
-    'contrast-1': 0.5563, 'contrast-2': 0.4654, 'contrast-3': 0.3937, 'contrast-4': 0.3238, 'contrast-5': 0.27,
-    'jpeg-1': 0.604, 'jpeg-2': 0.603, 'jpeg-3': 0.6022, 'jpeg-4': 0.5957, 'jpeg-5': 0.5645
-}
-results_vgg11 = {
-    'none': 0.7599,
-    'gaussian_noise-1': 0.7296, 'gaussian_noise-2': 0.6509, 'gaussian_noise-3': 0.5441, 'gaussian_noise-4': 0.3746, 'gaussian_noise-5': 0.167,
-    'gaussian_blur-1': 0.7604, 'gaussian_blur-2': 0.7425, 'gaussian_blur-3': 0.7041, 'gaussian_blur-4': 0.6131, 'gaussian_blur-5': 0.4812,
-    'contrast-1': 0.745, 'contrast-2': 0.7021, 'contrast-3': 0.6472, 'contrast-4': 0.5591, 'contrast-5': 0.4281,
-    'jpeg-1': 0.7584, 'jpeg-2': 0.7508, 'jpeg-3': 0.7449, 'jpeg-4': 0.7327, 'jpeg-5': 0.6602
-}
-results_vgg11_bn = {
-    'none': 0.759,
-    'gaussian_noise-1': 0.6878, 'gaussian_noise-2': 0.5738, 'gaussian_noise-3': 0.4624, 'gaussian_noise-4': 0.3255, 'gaussian_noise-5': 0.1922,
-    'gaussian_blur-1': 0.7609, 'gaussian_blur-2': 0.7454, 'gaussian_blur-3': 0.6962, 'gaussian_blur-4': 0.5945, 'gaussian_blur-5': 0.4532,
-    'contrast-1': 0.7453, 'contrast-2': 0.6887, 'contrast-3': 0.6142, 'contrast-4': 0.5023, 'contrast-5': 0.3853,
-    'jpeg-1': 0.7479, 'jpeg-2': 0.7387, 'jpeg-3': 0.7275, 'jpeg-4': 0.7114, 'jpeg-5': 0.6257
-}
+results_resnet18_epochs250 = {'none': 0.6597, 'gaussian_noise-1': 0.6498, 'gaussian_noise-2': 0.6309, 'gaussian_noise-3': 0.6008, 'gaussian_noise-4': 0.5411, 'gaussian_noise-5': 0.4189, 'gaussian_blur-1': 0.6624, 'gaussian_blur-2': 0.6502, 'gaussian_blur-3': 0.6187, 'gaussian_blur-4': 0.5547, 'gaussian_blur-5': 0.4563, 'contrast-1': 0.6257, 'contrast-2': 0.5471, 'contrast-3': 0.463, 'contrast-4': 0.3661, 'contrast-5': 0.2917, 'jpeg-1': 0.6548, 'jpeg-2': 0.6494, 'jpeg-3': 0.6489, 'jpeg-4': 0.6434, 'jpeg-5': 0.5978}
+results_resnet34_epochs250 = {'none': 0.62, 'gaussian_noise-1': 0.6109, 'gaussian_noise-2': 0.6003, 'gaussian_noise-3': 0.5789, 'gaussian_noise-4': 0.5369, 'gaussian_noise-5': 0.436, 'gaussian_blur-1': 0.6194, 'gaussian_blur-2': 0.6108, 'gaussian_blur-3': 0.5945, 'gaussian_blur-4': 0.542, 'gaussian_blur-5': 0.4399, 'contrast-1': 0.5723, 'contrast-2': 0.4865, 'contrast-3': 0.4003, 'contrast-4': 0.3182, 'contrast-5': 0.2448, 'jpeg-1': 0.6196, 'jpeg-2': 0.6161, 'jpeg-3': 0.6115, 'jpeg-4': 0.6085, 'jpeg-5': 0.5686}
+results_vgg11_epochs250 = {'none': 0.777, 'gaussian_noise-1': 0.749, 'gaussian_noise-2': 0.6665, 'gaussian_noise-3': 0.5537, 'gaussian_noise-4': 0.3593, 'gaussian_noise-5': 0.1523, 'gaussian_blur-1': 0.7796, 'gaussian_blur-2': 0.7653, 'gaussian_blur-3': 0.7285, 'gaussian_blur-4': 0.6484, 'gaussian_blur-5': 0.5204, 'contrast-1': 0.7753, 'contrast-2': 0.7545, 'contrast-3': 0.7136, 'contrast-4': 0.6313, 'contrast-5': 0.4812, 'jpeg-1': 0.7708, 'jpeg-2': 0.761, 'jpeg-3': 0.7551, 'jpeg-4': 0.738, 'jpeg-5': 0.6532}
+results_vgg11_bn_epochs250 = {'none': 0.7774, 'gaussian_noise-1': 0.717, 'gaussian_noise-2': 0.6037, 'gaussian_noise-3': 0.4878, 'gaussian_noise-4': 0.3344, 'gaussian_noise-5': 0.1813, 'gaussian_blur-1': 0.7793, 'gaussian_blur-2': 0.7621, 'gaussian_blur-3': 0.7179, 'gaussian_blur-4': 0.6152, 'gaussian_blur-5': 0.4755, 'contrast-1': 0.7616, 'contrast-2': 0.7098, 'contrast-3': 0.6442, 'contrast-4': 0.5369, 'contrast-5': 0.4109, 'jpeg-1': 0.7688, 'jpeg-2': 0.7525, 'jpeg-3': 0.7406, 'jpeg-4': 0.7268, 'jpeg-5': 0.6414}
+
 results_densenet121 = {
     'none': 0.668,
     'gaussian_noise-1': 0.6526, 'gaussian_noise-2': 0.5974, 'gaussian_noise-3': 0.5222, 'gaussian_noise-4': 0.4027, 'gaussian_noise-5': 0.2362,
@@ -50,7 +25,7 @@ legend_kwargs = {
 
 
 def compute_ce(relative: bool =False):
-    model_data = [results_resnet18, results_resnet34, results_vgg11_bn, results_vgg11, results_densenet121]
+    model_data = [results_resnet18_epochs250, results_resnet34_epochs250, results_vgg11_bn_epochs250, results_vgg11_epochs250, results_densenet121]
     for c in ['gaussian_noise', 'gaussian_blur', 'contrast', 'jpeg']:
         c_points = []
         for data in model_data:
@@ -58,10 +33,10 @@ def compute_ce(relative: bool =False):
             nominator = 0
             for s in range(1, 6):
                 nominator += (1 - data[f"{c}-{s}"])
-                denominator += (1 - results_resnet18[f"{c}-{s}"])
+                denominator += (1 - results_resnet18_epochs250[f"{c}-{s}"])
                 if relative:
                     nominator -= (1 - data['none'])
-                    denominator -= (1 - results_resnet18['none'])
+                    denominator -= (1 - results_resnet18_epochs250['none'])
             c_points.append(nominator/denominator)
 
         x_point = np.arange(1, len(model_data) + 1)
@@ -85,33 +60,33 @@ def graph_resnet18():
     plt.ylabel("Accuracy")
     plt.title("Accuracy loss by image distortion method")
     plt.plot(x_point, [
-        results_resnet18['gaussian_blur-1'],
-        results_resnet18['gaussian_blur-2'],
-        results_resnet18['gaussian_blur-3'],
-        results_resnet18['gaussian_blur-4'],
-        results_resnet18['gaussian_blur-5']
+        results_resnet18_epochs250['gaussian_blur-1'],
+        results_resnet18_epochs250['gaussian_blur-2'],
+        results_resnet18_epochs250['gaussian_blur-3'],
+        results_resnet18_epochs250['gaussian_blur-4'],
+        results_resnet18_epochs250['gaussian_blur-5']
     ], label="Gaussian blur")
     plt.xticks(x_point)
     plt.plot(x_point, [
-        results_resnet18['gaussian_noise-1'],
-        results_resnet18['gaussian_noise-2'],
-        results_resnet18['gaussian_noise-3'],
-        results_resnet18['gaussian_noise-4'],
-        results_resnet18['gaussian_noise-5']
+        results_resnet18_epochs250['gaussian_noise-1'],
+        results_resnet18_epochs250['gaussian_noise-2'],
+        results_resnet18_epochs250['gaussian_noise-3'],
+        results_resnet18_epochs250['gaussian_noise-4'],
+        results_resnet18_epochs250['gaussian_noise-5']
     ], label="Gaussian noise")
     plt.plot(x_point, [
-        results_resnet18['jpeg-1'],
-        results_resnet18['jpeg-2'],
-        results_resnet18['jpeg-3'],
-        results_resnet18['jpeg-4'],
-        results_resnet18['jpeg-5']
+        results_resnet18_epochs250['jpeg-1'],
+        results_resnet18_epochs250['jpeg-2'],
+        results_resnet18_epochs250['jpeg-3'],
+        results_resnet18_epochs250['jpeg-4'],
+        results_resnet18_epochs250['jpeg-5']
     ], label="JPEG compression loss")
     plt.plot(x_point, [
-        results_resnet18['contrast-1'],
-        results_resnet18['contrast-2'],
-        results_resnet18['contrast-3'],
-        results_resnet18['contrast-4'],
-        results_resnet18['contrast-5']
+        results_resnet18_epochs250['contrast-1'],
+        results_resnet18_epochs250['contrast-2'],
+        results_resnet18_epochs250['contrast-3'],
+        results_resnet18_epochs250['contrast-4'],
+        results_resnet18_epochs250['contrast-5']
     ], label="Contrast")
     plt.legend(**legend_kwargs)
     plt.show()
